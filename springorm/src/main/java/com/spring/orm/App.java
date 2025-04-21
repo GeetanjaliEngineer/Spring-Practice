05c9d113d9960aa26397cf16ea1b2c9f26165a21
geetanjali <165178774+GeetanjaliEngineer@users.noreply.github.com>
2025-04-21 20:35:24 +0530

    Add files via upload
    This is project Spring core+Spring orm and hibernate with database

--- springorm/src/main/java/com/spring/orm/App.java
+++ springorm/src/main/java/com/spring/orm/App.java
@@ -2,6 +2,7 @@ package com.spring.orm;
 
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
+import java.util.List;
 
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
@@ -43,23 +44,88 @@ public class App {
 				switch (input) {
 				case 1:
 					// add a new student
+					// taking input from user
+					System.out.println("Enter user Id: ");
+					int uid = Integer.parseInt(br.readLine());
 
+					System.out.println("Enter User Name: ");
+					String uName = br.readLine();
+
+					System.out.println("Enter user city: ");
+					String uCity = br.readLine();
+					// creating student object
+					Student s = new Student();
+					s.setStudentId(uid);
+					s.setStudentName(uName);
+					s.setStudentCity(uCity);
+					// saving studetn object to database by calling insert of studentDao
+					int r = studentDao.insert(s);
+					System.out.println(r + " Student added");
+					System.out.println("____________________________________");
 					break;
+
 				case 2:
 					// display all student
+
+					List<Student> allStudents = studentDao.getAllStudents();
+					for (Student st : allStudents) {
+						System.out.println("Name: " + st.getStudentName());
+						System.out.println("Id: " + st.getStudentId());
+						System.out.println("City" + st.getStudentCity());
+						System.out.println("________________________________");
+					}
 					break;
 				case 3:
 					// get a single student data
+					System.out.println("Enter User id: ");
+					int userId = Integer.parseInt(br.readLine());
+					Student student = studentDao.getStudent(userId);
+					System.out.println("Id :" + student.getStudentId());
+					System.out.println("Name : " + student.getStudentName());
+					System.out.println("City :" + student.getStudentCity());
+
 					break;
 				case 4:
 					// delete single student
+					System.out.println("Enter user id :");
+					int id = Integer.parseInt(br.readLine());
+					studentDao.deleteStudent(id);
+					System.out.println("Student deleted successfully..");
+					System.out.println("___________________________________________");
+
 					break;
 				case 5:
 					// update the student
+					System.out.println("Enter Student id to update:");
+					int uid1 = Integer.parseInt(br.readLine());
+					Student student2 = studentDao.getStudent(uid1);
+					if (student2 != null) {
+						System.out.println("Current Name: " + student2.getStudentName());
+						System.out.println("Enter New Name:");
+						String newName = br.readLine();
+
+						System.out.println("Current City: " + student2.getStudentCity());
+						System.out.println("Enter New City: ");
+						String newCity = br.readLine();
+
+						student2.setStudentName(newName);
+						student2.setStudentCity(newCity);
+
+						studentDao.updateStudent(student2);
+						System.out.println("Student Updated successfully.");
+					} else {
+						System.out.println("Student with ID" + uid1 + "does not exist.");
+
+					}
+					System.out.println("________________________________________");
+
 					break;
 				case 6:
 					// exit
+
 					go = false;
+					System.out.println("Exiting the application...");
+					System.out.println("GoodBye!");
 
 					break;
 
